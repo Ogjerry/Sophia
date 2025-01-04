9c687ca87b2aac53fee3d0f3201fc22a014ec955
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2025-01-05 00:04:53 +0800

    fix Missing Suspense boundary with useSearchParams

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useState, useEffect } from "react";
+import { useState, useEffect,Suspense } from "react";
 
-export default function SearchPage() {
+interface SearchResult {
+  subtitle: string;
+  text: string;
+  url: string;
+}
+function Search() {
   const searchParams = useSearchParams();
   const search = searchParams.get("q");
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<SearchResult[]>([]);
   const [sortOrder, setSortOrder] = useState("relevance");
 
   useEffect(() => {
@@ -17,36 +22,33 @@ export default function SearchPage() {
     }
   }, [search, sortOrder]);
 
-  // TODO: Add lazy loading on the backend
-
   return (
     <div className="container mx-auto p-4">
-      
       <div className="mb-4">
         <div className="flex items-center justify-between">
           <div>
             <h1 className="text-2xl font-bold mr-2">Search Results for &ldquo;{search}&rdquo;</h1>
-            </div>
-            <div>
-              <label className="mr-2">Sort by:</label>
-              <select
+          </div>
+          <div>
+            <label className="mr-2">Sort by:</label>
+            <select
               className="border border-gray-300 rounded p-2"
               value={sortOrder}
               onChange={(e) => setSortOrder(e.target.value)}
-        >
-          <option value="relevance">Relevance</option>
-          <option value="latest">Latest</option>
-          <option value="oldest">Oldest</option>
-        </select>
+            >
+              <option value="relevance">Relevance</option>
+              <option value="latest">Latest</option>
+              <option value="oldest">Oldest</option>
+            </select>
+          </div>
         </div>
-      </div>
       </div>
       <div>
         {results.length > 0 ? (
           results.map((result, index) => (
             <div key={index} className="mb-4 p-4 border border-gray-300 rounded">
               <h2 className="text-xl font-bold">{result.subtitle}</h2>
-              <p dangerouslySetInnerHTML={{ __html: result.text.replace(new RegExp(search, 'gi'), (match) => `<mark>${match}</mark>`) }}></p>
+              <p dangerouslySetInnerHTML={{ __html: result.text.replace(new RegExp(search || '', 'gi'), (match: string) => `<mark>${match}</mark>`) }}></p>
               <a href={"/wiki/" + result.url} className="text-blue-500 hover:underline">
                 Read more
               </a>
@@ -57,5 +59,13 @@ export default function SearchPage() {
         )}
       </div>
     </div>
+  );
+}
+
+export default function SearchPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Search />
+    </Suspense>
   );
 }
